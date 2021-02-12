8ce6204540a36b795b79c958dc83fa734fb05220
andrey.pylypchuk <andrey.pylypchuk@introlab-systems.com>
2021-02-12 17:50:37 +0200

    create InterpretationEntity created InterpretationRelationship joined InterpretationEntity with QuestionExecutionEntity by relationship removed unused relationship

--- checklist-app/src/main/java/com/datapath/checklistapp/dao/entity/QuestionExecutionEntity.java
+++ checklist-app/src/main/java/com/datapath/checklistapp/dao/entity/QuestionExecutionEntity.java
@@ -1,5 +1,6 @@
 package com.datapath.checklistapp.dao.entity;
 
+import com.datapath.checklistapp.dao.relatioship.InterpretationRelationship;
 import lombok.Data;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
@@ -18,6 +19,9 @@ public class QuestionExecutionEntity {
     private Integer conditionAnswerId;
 
     private Integer orderNumber;
+
+    @Relationship(type = "HAS_INTERPRETATION")
+    private InterpretationRelationship interpretation;
 
     @Relationship(type = "OF_QUESTION")
     private QuestionEntity question;
